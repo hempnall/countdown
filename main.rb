cae7d9f71988190a9eb3b89826095a2b9195c12b
hempnall <hempnall@yahoo.co.uk>
2014-02-06 17:31:19 +0000

    slight tweaking

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 numbers = [100,2,75,3,7,8]
-target = 863
+target = 98
 
 
 def remove_numbers(arr,numa,numb)
@@ -26,10 +26,12 @@ def evaluate_array(arr,newitem,targ,leng,opstack,op)
 
 	# we need a better way of detecting whether we have reached the target - i dont think 
 	# this will catch every case :-(
-	if newitem[0] == targ
+	if earr.index(targ)
 		puts "---------"
 		puts opstack.inspect
 		puts "---------"
+		opstack.pop
+		return
 	end
 
 
@@ -50,8 +52,8 @@ def evaluate_array(arr,newitem,targ,leng,opstack,op)
 		test = c[0] - c[1]
 		evaluate_array(next_array,[test],targ,leng-1,opstack,"#{c[0]} - #{c[1]} = #{test}")
 
-		test = c[1] - c[0]
-		evaluate_array(next_array,[test],targ,leng-1,opstack,"#{c[1]} - #{c[0]} = #{test}")
+		#test = c[1] - c[0]
+		#evaluate_array(next_array,[test],targ,leng-1,opstack,"#{c[1]} - #{c[0]} = #{test}")
 
 		if c[0] !=0 and c[1] % c[0] == 0  
 			test = c[1] / c[0]
